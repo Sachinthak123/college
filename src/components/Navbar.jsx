d82ed2691c0b2ed8667d6e62871d31c4866a9d4e
Sachinthak123 <132138718+Sachinthak123@users.noreply.github.com>
2026-07-30 17:13:24 +0530

    Fix nav menu mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -67,7 +67,7 @@ const Navbar = () => {
 
       {/* Mobile Sidebar */}
       <div
-        className={`fixed top-0 right-[27%] w-2/5 h-screen bg-blue-900 shadow-2xl z-50 transform transition-transform duration-300 lg:hidden ${
+        className={`fixed top-0 right-0 w-[70%] sm:w-[50%] md:w-[40%] h-screen bg-blue-900 shadow-2xl z-50 transform transition-transform duration-300 ${
           menuOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
